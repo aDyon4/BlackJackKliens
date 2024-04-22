a5919c4649bd2520f36eefe726868d10ca47f75d
Patrik Adam <adam.patrik.sztk@gmail.com>
2024-04-22 21:46:06 +0200

    kliens fejlesztes 2.0

--- src/main/java/com/example/bjkliens/HelloController.java
+++ src/main/java/com/example/bjkliens/HelloController.java
@@ -6,23 +6,38 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
 
 import java.io.IOException;
 import java.net.*;
+import java.util.Stack;
 
 public class HelloController {
 
-    @FXML private TextField tfText;
+
+    @FXML private ImageView ivLap;
+    @FXML private Label lbPont;
     @FXML private ListView<String> lvList;
-    @FXML private TextField tfId;
     @FXML private Button exit;
     @FXML private Button bet;
     @FXML private Button join;
+    @FXML private TextField tfId;
+    @FXML private TextField tfText;
     @FXML private TextField tfBet;
 
     DatagramSocket socket = null;
+    Image[] lapok = new Image[8];
 
+    int randSzam = (int)(Math.random()*9)+2;
+    String[] randBetu = { "C", "D", "H", "S" };
+    int randBetuSzam = (int)(Math.random()*3);
+
+    Image lap = new Image(getClass().getResourceAsStream("lapok/"+randSzam+""+randBetu[randBetuSzam]+".png"));
+    Image lapHata = new Image(getClass().getResourceAsStream("lapok/gray_back.png"));
     public void initialize(){
+
         try {
             socket = new DatagramSocket(678);
         } catch (SocketException e) {
@@ -40,6 +55,7 @@ public class HelloController {
         join.setDisable(false);
         exit.setDisable(true);
         bet.setDisable(true);
+        tfBet.setDisable(true);
     }
 
     int osszesPent = 0;
@@ -48,17 +64,26 @@ public class HelloController {
         String zseton = "join:"+osszesPent;
         String id = tfId.getText();
         //System.out.printf("id: %s", id);
-        kuld(zseton, "10.201.2.5", 678);
+        kuld(zseton, "192.168.1.212", 688);
         exit.setDisable(false);
         bet.setDisable(false);
+        tfBet.setDisable(false);
         join.setDisable(true);
     }
 
     @FXML private void onBetClick(){
-        int tet = Integer.parseInt(tfBet.getText());
-        if(tet<=osszesPent){ kuld("bet:"+tet, "10.201.2.5", 678); }
-        else System.out.println("nincs ennyi pénzed");
 
+        int tet = Integer.parseInt(tfBet.getText());
+        if(tet<=osszesPent){
+            ivLap.setImage(lap);
+            lbPont.setText(randSzam+"");
+            kuld("bet:"+tet, "192.168.1.212", 688);
+        }
+        else System.out.println("nincs ennyi pénzed");
+    }
+
+    @FXML private void onPlusPressed(){
+            kuld("plus:"+100, "192.168.1.212", 688);
     }
 
     private void kuld(String uzenet, String ip, int port) {
@@ -74,9 +99,11 @@ public class HelloController {
     }
 
     @FXML private void onExitClick(){
-        kuld("exit", "10.201.2.5", 678);
+        kuld("exit", "192.168.1.212", 688);
+        ivLap.setImage(lapHata);
         exit.setDisable(true);
         bet.setDisable(true);
+        tfBet.setDisable(true);
         join.setDisable(false);
     }
 
@@ -86,7 +113,6 @@ public class HelloController {
         while (true){
             try {
                 socket.receive(packet);
-
                 String uzenet = new String(packet.getData(), 0, packet.getLength(), "utf-8");
                 String ip  = packet.getAddress().getHostAddress();
                 int port = packet.getPort();
